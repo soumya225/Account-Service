0b6a33248a22b556d0a06d787409c5a8796fce68
Soumya Satheesan <25707177+soumya225@users.noreply.github.com>
2022-07-05 12:24:57 +0400

    Create the service structure

---
+++ Account Service/task/src/account/UserDetails.java
@@ -0,0 +1,37 @@
+package account;
+
+public class UserDetails {
+    private String name;
+    private String lastname;
+    private String email;
+
+    public UserDetails(String name, String lastname, String email) {
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
